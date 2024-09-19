9e8f04504a7c9452feb69cd88b557d0d6bc9ab88
Dam Van Hoang Buu <buudvh@glotechvn.com>
2024-09-19 16:33:39 +0700

    add swagger

---
+++ RoadToSenior/RoadToSenior/Extensions/SwaggerServiceExtensions.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace RoadToSenior.Extensions
+{
+    public static class SwaggerServiceExtensions
+    {
+        public static IServiceCollection AddSwaggerDocumentation(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "API Documentation",
+                    Description = "API dành cho các dịch vụ",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Road To Senior",
+                        Email = "damhoangbuu@gmail.com",
+                        Url = new Uri("https://example.com")
+                    }
+                });
+
+                // Cấu hình JWT Authorization cho Swagger nếu cần
+                //c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                //{
+                //    In = ParameterLocation.Header,
+                //    Description = "Vui lòng nhập JWT theo định dạng: Bearer {token}",
+                //    Name = "Authorization",
+                //    Type = SecuritySchemeType.ApiKey,
+                //    Scheme = "Bearer"
+                //});
+
+                //c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                //{
+                //    {
+                //        new OpenApiSecurityScheme
+                //        {
+                //            Reference = new OpenApiReference
+                //            {
+                //                Type = ReferenceType.SecurityScheme,
+                //                Id = "Bearer"
+                //            }
+                //        },
+                //        Array.Empty<string>()
+                //    }
+                //});
+            });
+
+            return services;
+        }
+    }
+}
--- RoadToSenior/RoadToSenior/Program.cs
+++ RoadToSenior/RoadToSenior/Program.cs
@@ -1,3 +1,5 @@
+﻿using Microsoft.OpenApi.Models;
+using RoadToSenior.Extensions;
 using RoadToSenior.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,15 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Thêm Swagger service
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerDocumentation();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    //app.UseExceptionHandler("/Home/Error");
+    //// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSwaggerDocumentation(app.Environment);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
