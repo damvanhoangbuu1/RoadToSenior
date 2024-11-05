1ee460ce686b5f9446bc5e2f08a7d31c7fa81cc7
Dam Van Hoang Buu <buudvh@glotechvn.com>
2024-11-05 14:47:54 +0700

    add Migrations

--- CleanArchitectureTemplate/3.CleanArchitecture.Infrastructure/Migrations/20241105074426_Update.Designer.cs
+++ CleanArchitectureTemplate/3.CleanArchitecture.Infrastructure/Migrations/20241105074426_Update.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using _3.CleanArchitecture.Infrastructure.Persistence;
@@ -12,9 +13,10 @@ using _3.CleanArchitecture.Infrastructure.Persistence;
 namespace _3.CleanArchitecture.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241105074426_Update")]
+    partial class Update
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CleanArchitectureTemplate/3.CleanArchitecture.Infrastructure/Migrations/20241105074426_Update.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _3.CleanArchitecture.Infrastructure.Migrations
+{
+    public partial class Update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+        }
+    }
+}
